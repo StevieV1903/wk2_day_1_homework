802f8bd6ad46586b2f100663b63888d471155979
Steve Vance <steve.vance@hotmail.co.uk>
2019-11-25 15:32:35 +0000

    Part A completed

--- codeclan_student.rb
+++ codeclan_student.rb
@@ -1,8 +1,9 @@
 class CodeclanStudent
 
-def initialize(student_name, cohort)
+def initialize(student_name, cohort, language)
   @student_name = student_name
   @cohort = cohort
+  @language = language
 end
 
 def student_name
@@ -11,6 +12,22 @@ end
 
 def student_cohort
   return @cohort
+end
+
+def set_student_name(new_name)
+@student_name = new_name
+end
+
+def set_cohort(new_cohort)
+  @cohort = new_cohort
+end
+
+def make_student_talk(phrase)
+  return phrase
+end
+
+def favourite_language()
+  return "I love #{@language}"
 end
 
 
