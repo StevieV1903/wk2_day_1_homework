59dc0a2871230060551a833d726b3b51c4dbc88c
Steve Vance <steve.vance@hotmail.co.uk>
2019-11-25 21:40:30 +0000

    most of part B

--- codeclan_student.rb
+++ codeclan_student.rb
@@ -1,43 +1,98 @@
-class CodeclanStudent
+class SportsTeam
 
-def initialize(student_name, cohort, language)
-  @student_name = student_name
-  @cohort = cohort
-  @language = language
+  attr_reader :team_name, :players
+  attr_accessor :coach
+
+  def initialize(team_name, players, coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
+
+  end
+
+  # def get_team_name
+  #   return @team_name
+  # end
+
+  # def get_players
+  #   return @players
+  # end
+  #
+  # def get_coach_name
+  #   return @coach
+  # end
+  #
+  # def set_coach(new_name)
+  #   @coach = new_name
+  # end
+
+def add_new_player(new_player)
+  @players << new_player
 end
 
-def student_name
-return @student_name
-end
-
-def student_cohort
-  return @cohort
-end
-
-def set_student_name(new_name)
-@student_name = new_name
-end
-
-def set_cohort(new_cohort)
-  @cohort = new_cohort
-end
-
-def make_student_talk(phrase)
-  return phrase
-end
-
-def favourite_language()
-  return "I love #{@language}"
+def find_player_by_name(name)
+  for player_name in @players
+    if player_name == name
+      player_found = player_name
+    end
+  end
+  return player_found
 end
 
 
 
 
 
-
-
-
-
-
-
 end
+
+
+
+#HOMEWORK PART A ;
+
+# class CodeclanStudent
+#
+# def initialize(student_name, cohort, language)
+#   @student_name = student_name
+#   @cohort = cohort
+#   @language = language
+# end
+#
+# def student_name
+# return @student_name
+# end
+#
+# def student_cohort
+#   return @cohort
+# end
+#
+# def set_student_name(new_name)
+# @student_name = new_name
+# end
+#
+# def set_cohort(new_cohort)
+#   @cohort = new_cohort
+# end
+#
+# def make_student_talk(phrase)
+#   return phrase
+# end
+#
+# def favourite_language()
+#   return "I love #{@language}"
+# end
+#
+
+# Homework PART B...
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# end
--- specs/codeclan_student_spec.rb
+++ specs/codeclan_student_spec.rb
@@ -3,40 +3,62 @@ require('minitest/reporters')
 require_relative('../codeclan_student')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-class TestCodeclanStudent < MiniTest::Test
+class TestSportsTeam < MiniTest::Test
 
-  def test_can_get_student_name
-student = CodeclanStudent.new("Jen", "E36", "Ruby")
-assert_equal("Jen", student.student_name())
+
+
+  def setup
+    @team_name = "Edinburgh"
+    @players = ["Jen", "Steve"]
+    @coach = "Alex"
+    @points = 0
   end
 
-  def test_can_get_cohort
-student = CodeclanStudent.new("Steve", "E37", "Ruby")
-assert_equal("E37", student.student_cohort())
-end
+  def test_get_team_name
+    team = SportsTeam.new(@team_name, @players, @coach)
+    assert_equal("Edinburgh", team.team_name())
+  end
 
-def test_can_set_student_name
-  student = CodeclanStudent.new("Jen", "E36", "Ruby")
-  student.set_student_name("Erin")
-  assert_equal("Erin", student.student_name())
-end
+  def test_get_players_name
+    team = SportsTeam.new(@team_name, @players, @coach)
+    assert_equal(["Jen", "Steve"], team.players())
+  end
 
-def test_can_set_cohort
-  student = CodeclanStudent.new("Alex", "E35","Ruby")
-  student.set_cohort("E34")
-  assert_equal("E34", student.student_cohort())
-end
+  def test_coach_name
+    team = SportsTeam.new(@team_name, @players, @coach)
+    assert_equal("Alex", team.coach())
+  end
 
-def test_can_make_student_talk
-  student = CodeclanStudent.new("James", "E36","Ruby")
-  phrase = "I can talk!"
-  assert_equal("I can talk!", student.make_student_talk(phrase))
-end
+  def test_set_coach_name
+    team = SportsTeam.new(@team_name, @players, @coach)
+    team.coach = "John"
+    assert_equal("John", team.coach())
+  end
 
-def test_get_student_favourite_language
-  student = CodeclanStudent.new("James", "E36", "Ruby")
-  assert_equal("I love Ruby", student.favourite_language)
-end
+  def test_add_new_player
+    team = SportsTeam.new(@team_name, @players, @coach)
+    team.add_new_player("Bob")
+    assert_equal(3, @players.length)
+  end
+
+  def test_find_player_by_name
+    team = SportsTeam.new(@team_name, @players, @coach)
+    player_name = find_player_by_name(@players)
+    assert_equal(true, team.players())
+  end
+
+  def test_find_player_by_name
+    team = SportsTeam.new(@team_name, @players, @coach)
+    assert_equal("Steve", team.find_player_by_name("Steve"))
+  end
+
+  # def test_add_points_if_won
+  #     team = SportsTeam.new(@team_name, @players, @coach, @points)
+  #     assert_equal(1, team.won_or_lost("win"))
+  #   end
+
+    
+  end
 
 
 
@@ -46,14 +68,40 @@ end
 
 
 
+#HOMEWORK PART A;
 
-
-
-
-
-
-
-
-
-
-end
+# class TestCodeclanStudent < MiniTest::Test
+#
+#   def test_can_get_student_name
+# student = CodeclanStudent.new("Jen", "E36", "Ruby")
+# assert_equal("Jen", student.student_name())
+#   end
+#
+#   def test_can_get_cohort
+# student = CodeclanStudent.new("Steve", "E37", "Ruby")
+# assert_equal("E37", student.student_cohort())
+# end
+#
+# def test_can_set_student_name
+#   student = CodeclanStudent.new("Jen", "E36", "Ruby")
+#   student.set_student_name("Erin")
+#   assert_equal("Erin", student.student_name())
+# end
+#
+# def test_can_set_cohort
+#   student = CodeclanStudent.new("Alex", "E35","Ruby")
+#   student.set_cohort("E34")
+#   assert_equal("E34", student.student_cohort())
+# end
+#
+# def test_can_make_student_talk
+#   student = CodeclanStudent.new("James", "E36","Ruby")
+#   phrase = "I can talk!"
+#   assert_equal("I can talk!", student.make_student_talk(phrase))
+# end
+#
+# def test_get_student_favourite_language
+#   student = CodeclanStudent.new("James", "E36", "Ruby")
+#   assert_equal("I love Ruby", student.favourite_language)
+# end
+# end
