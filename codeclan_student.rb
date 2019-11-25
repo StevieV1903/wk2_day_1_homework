55e06ea595b40be7d60a4370e4604b24152e54e2
Steve Vance <steve.vance@hotmail.co.uk>
2019-11-25 14:37:18 +0000

    passed student name test

---
+++ codeclan_student.rb
@@ -0,0 +1,22 @@
+class CodeclanStudent
+
+def initialize(student_name, cohort)
+  @student_name = student_name
+  @cohort = cohort
+end
+
+def student_name
+return @student_name
+end 
+
+
+
+
+
+
+
+
+
+
+
+end
